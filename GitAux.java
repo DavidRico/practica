6e4ae643ea8f2fc8d8d88b3f480eac539839ed5d
nur-ag <nurloco@hotmail.com>
2013-03-04 18:24:55 +0100

    Modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -8,18 +8,21 @@ package prgitejemplo;
  * @author davidrico
  */
 public class GitAux {
-    
+
     public void metodoAlumno1(){
         System.out.println("metodo 1");
     }
-    
+
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+        System.out.println("Alumno 2: cambios"); // Modificación del método de alumno 2
     }
-    
+
     public void metodoComunitario(){
         //comentario al bulto
         System.out.println("Aqui escribimos todos");
+        //Cambios de alumno 2
+        System.out.println("Alumno 2");
     }
 
     public void testGitAux(){
